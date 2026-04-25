877f9b526b6f0522f8e708594f8744479d76b884
unknown <tamiloreolaniyi@gmail.com>
2026-04-25 21:20:46 +0100

    admin fix

--- vite.config.js
+++ vite.config.js
@@ -2,9 +2,9 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(),tailwindcss()],
+  base: '/',
+  plugins: [react(), tailwindcss()],
   server: {
     proxy: {
       '/api': {
